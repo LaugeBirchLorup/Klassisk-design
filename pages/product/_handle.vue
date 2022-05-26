68d57247c442959ed506385d44506b6d4ac1bc32
LaugeBirchLorup <Laugebirch@gmail.com>
2022-05-26 21:01:16 +0200

    update _handle (building product page)

--- pages/product/_handle.vue
+++ pages/product/_handle.vue
@@ -1,6 +1,14 @@
 <template>
-  <div class="text-center">
-    {{ product.title }}
+  <div class="container page-container">
+    <div class="image-container">
+      <img class="rounded-md" :src="product.images[0].src" alt="" />
+    </div>
+    <div class="content-contianer">
+      <h3 class="category">Stol</h3>
+      <h1 class="main-title">{{ product.title }}</h1>
+      <p class="description">{{ product.description }}</p>
+      <h2>{{ product.variants[0].price }}</h2>
+    </div>
   </div>
 </template>
 
